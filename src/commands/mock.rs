2ad09f5c21d817374fa34cfd6490f7e17de3ce26
Thomas Campistron <irevoire@hotmail.fr>
2020-04-11 21:01:10 +0200

    mock is not utf-8 compliant

--- src/commands/mock.rs
+++ src/commands/mock.rs
@@ -1,25 +1,20 @@
-
-
 use serenity::framework::standard::{macros::command, Args, CommandResult};
 
-use serenity::{
-    model::channel::Message,
-    prelude::{Context},
-};
+use serenity::{model::channel::Message, prelude::Context};
 
 #[command]
 pub fn mock(ctx: &mut Context, msg: &Message, _args: Args) -> CommandResult {
     let mut new = String::new();
     let mut last = false;
-    for c in msg.content.chars().skip("!mock".len()) {
-        if !c.is_ascii_alphabetic() {
+    for c in msg.content.chars().skip("!mock".chars().count()) {
+        if !c.is_alphabetic() {
             new.push(c);
             continue;
         }
         if last {
-            new.push(c.to_ascii_lowercase());
+            new.push_str(&c.to_lowercase().to_string());
         } else {
-            new.push(c.to_ascii_uppercase());
+            new.push_str(&c.to_uppercase().to_string());
         }
         last = !last;
     }

784cd0ab4e97033bf1a13a82720923403139f624
Thomas Campistron <thomas.campistron@huawei.com>
2020-04-13 13:34:35 +0200

    upgrade the mock command to send images

--- src/commands/mock.rs
+++ src/commands/mock.rs
@@ -1,6 +1,27 @@
 use serenity::framework::standard::{macros::command, Args, CommandResult};
-
 use serenity::{model::channel::Message, prelude::Context};
+use std::env;
+
+fn get_image_url(text: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let logs = env::var("IMGFLIP")?;
+    let logs: Vec<&str> = logs.splitn(2, ':').collect();
+    let username = logs[0];
+    let password = logs[1];
+
+    let url = format!(
+        "username={}&password={}&template_id=102156234&boxes[0][text]=&boxes[1][text]={}",
+        username, password, text
+    );
+    let resp = ureq::post("https://api.imgflip.com/caption_image")
+        .set("Content-Type", "application/x-www-form-urlencoded")
+        .send_string(&url);
+    let url = &resp.into_json()?["data"]["url"];
+    if let Some(url) = url.as_str() {
+        Ok(url.to_string())
+    } else {
+        Err("Could not as str".into())
+    }
+}
 
 #[command]
 pub fn mock(ctx: &mut Context, msg: &Message, _args: Args) -> CommandResult {
@@ -18,8 +39,14 @@ pub fn mock(ctx: &mut Context, msg: &Message, _args: Args) -> CommandResult {
         }
         last = !last;
     }
-    let _ = msg.reply(&ctx, new);
-    let _ = msg.delete(&ctx);
 
+    if let Ok(url) = get_image_url(&new) {
+        let _ = msg
+            .channel_id
+            .send_files(&ctx, vec![url.as_str()], |m| m.content(&msg.author));
+    } else {
+        let _ = msg.reply(&ctx, new);
+    }
+    let _ = msg.delete(&ctx);
     Ok(())
 }

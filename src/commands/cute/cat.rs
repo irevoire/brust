77df3bb40ffeaf154e2e3f3ba4486447c4d134d2
irevoire <irevoire@hotmail.fr>
2020-07-02 19:50:54 +0200

    better error handling for the cat command

--- src/commands/cute/cat.rs
+++ src/commands/cute/cat.rs
@@ -8,21 +8,45 @@ use serenity::{model::channel::Message, prelude::Context};
 #[command]
 #[description = "Send cute cat picture stolen from http://random.cat"]
 pub fn cat(ctx: &mut Context, msg: &Message, _args: Args) -> CommandResult {
-    let mut rng = rand::thread_rng();
-    let cat_id = rng.gen_range(0, 1677);
-    let page: String = Client::new()
-        .get(&format!("http://random.cat/view/{}", cat_id))
-        .send()?
-        .text()?;
-    let document = Document::from(page.as_str());
-    let url = document
-        .find(Attr("id", "cat"))
-        .next()
-        .unwrap()
-        .attr("src")
-        .unwrap();
+    let page = fetch_cat_page();
+    if let Err(e) = page {
+        let _ = msg.reply(
+            &ctx,
+            format!(
+                "Catto express: was not able to deliver you cat: {}\n{}",
+                e,
+                "https://i.redd.it/4q32jedhkgi31.jpg" // crying catto
+            ),
+        );
+        return Ok(());
+    }
+
+    let url = fetch_url_in_cat_page(page?);
+    if url.is_none() {
+        let _ = msg.reply(
+            &ctx,
+            format!("Catto express: your catto got lost in the page :pensive:"),
+        );
+        return Ok(());
+    }
+    let url = url.unwrap();
+
     let _ = msg
         .channel_id
-        .send_files(&ctx, vec![url], |m| m.content(&msg.author));
+        .send_files(&ctx, vec![url.as_str()], |m| m.content(&msg.author));
     Ok(())
+}
+
+fn fetch_cat_page() -> Result<String, Box<dyn std::error::Error>> {
+    let mut rng = rand::thread_rng();
+    let cat_id = rng.gen_range(0, 1677);
+    Ok(Client::new()
+        .get(&format!("http://random.cat/view/{}", cat_id))
+        .send()?
+        .text()?)
+}
+
+fn fetch_url_in_cat_page(page: String) -> Option<String> {
+    let document = Document::from(page.as_str());
+    Some(document.find(Attr("id", "cat")).next()?.attr("src")?.into())
 }

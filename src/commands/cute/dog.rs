c028be589287e900bedd07ee24ee18fa18b48595
irevoire <irevoire@hotmail.fr>
2020-07-09 18:31:25 +0200

    update the dog command to choose some breeds, need improvment

--- src/commands/cute/dog.rs
+++ src/commands/cute/dog.rs
@@ -7,31 +7,45 @@ use serenity::{model::channel::Message, prelude::Context};
 #[command]
 #[aliases("doggo")]
 #[description = "Send cute dog picture stolen from https://random.dog"]
-pub fn dog(ctx: &mut Context, msg: &Message, _args: Args) -> CommandResult {
-    let page = fetch_dog_page();
-    if let Err(e) = page {
-        let _ = msg.reply(
-            &ctx,
-            format!(
-                "Doggo express: your doggo was not able to come because: {}",
-                e
-            ),
-        );
-        return Ok(());
-    }
-    let url = fetch_url_in_dog_page(page?);
-    if url.is_none() {
-        let _ = msg.reply(
-            &ctx,
-            format!("Doggo express: your doggo got lost :pensive:"),
-        );
-        return Ok(());
-    }
-    let url = url.unwrap();
-    let _ = msg
-        .channel_id
-        .send_files(&ctx, vec![url.as_str()], |m| m.content(&msg.author));
+pub fn dog(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
+    let url = if args.len() != 0 {
+        fetch_dog_breed_url(args.raw().collect::<Vec<&str>>())
+    } else {
+        fetch_random_dog_url()
+    };
+    match url {
+        Ok(url) => msg
+            .channel_id
+            .send_files(&ctx, vec![url.as_str()], |m| m.content(&msg.author)),
+        Err(annoncement) => msg.reply(&ctx, format!("Doggo express: {}", annoncement)),
+    };
     Ok(())
+}
+
+/// look for a specific breed of dog from dog api
+fn fetch_dog_breed_url(mut breed: Vec<&str>) -> Result<String, Box<dyn std::error::Error>> {
+    breed.reverse();
+    let breed = breed.join("/");
+    let resp: serde_json::Value = Client::new()
+        .get(&format!(
+            "https://dog.ceo/api/breed/{}/images/random",
+            breed
+        ))
+        .send()?
+        .json()?;
+
+    match resp["status"].as_str().unwrap() {
+        "error" => Err(resp["message"].as_str().unwrap().into()),
+        "success" => Ok(resp["message"].as_str().unwrap().to_string()),
+        _ => Err("The doggo center looks closed".into()),
+    }
+}
+
+/// return an url from http://random.dog
+fn fetch_random_dog_url() -> Result<String, Box<dyn std::error::Error>> {
+    let page = fetch_dog_page()?;
+    let url = fetch_url_in_dog_page(page).ok_or("your doggo got lost :pensive:")?;
+    Ok(url)
 }
 
 fn fetch_dog_page() -> Result<String, Box<dyn std::error::Error>> {

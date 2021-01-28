cc4e64c34b30270208d8e46a7dc5c3873bfa1ccc
irevoire <irevoire@protonmail.ch>
2021-01-28 23:24:17 +0100

    add support for the quote in the react command

--- src/utils.rs
+++ src/utils.rs
@@ -76,7 +76,7 @@ pub async fn find_relative_content(
 
 pub fn unicode_to_safe_ascii(c: char) -> Option<char> {
     match c {
-        'a'..='z' | 'A'..='Z' | '0'..='9' | ' ' | '.' | '!' | '?' | '*' | '#' | "'" => {
+        'a'..='z' | 'A'..='Z' | '0'..='9' | ' ' | '.' | '!' | '?' | '*' | '#' | '\'' => {
             Some(c.to_ascii_lowercase())
         }
         'á' | 'à' | 'â' | 'ä' | 'Á' | 'À' | 'Â' | 'Ä' => Some('a'),
@@ -86,7 +86,7 @@ pub fn unicode_to_safe_ascii(c: char) -> Option<char> {
         'ú' | 'ù' | 'û' | 'ü' | 'Ú' | 'Ù' | 'Û' | 'Ü' => Some('u'),
         'ç' | 'Ç' => Some('c'),
         '«' | '»' | '“' | '”' | '„' => Some('"'),
-        '’' => Some("'"),
+        '’' => Some('\''),
         _ => None,
     }
 }

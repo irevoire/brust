017f58b079854d7d330ac04f8a931194dd99f422
Martin Lemesle <martin.lemesle@iomentum.com>
2023-03-28 11:59:43 +0200

    Change an command in order to download then send the whole file

--- src/commands/an.rs
+++ src/commands/an.rs
@@ -10,28 +10,38 @@ use serenity::{
 pub async fn an(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
     let data = ctx.data.read().await;
 
-    crate::repeat_message!(ctx, msg, {
-        // we want to free the lock as soon as possible
-        let mut rng = data.get::<crate::Random>().unwrap().lock().await;
+    // crate::repeat_message!(ctx, msg, {
+    // we want to free the lock as soon as possible
+    let mut rng = data.get::<crate::Random>().unwrap().lock().await;
 
-        let resp: serde_json::Value =
-            reqwest::get("https://www.nosdeputes.fr/deputes/enmandat/json")
-                .await?
-                .json()
-                .await?;
+    let resp: serde_json::Value = reqwest::get("https://www.nosdeputes.fr/deputes/enmandat/json")
+        .await?
+        .json()
+        .await?;
 
-        if let serde_json::Value::Array(deputes) = &resp["deputes"] {
-            let index = rng.gen_range(0..deputes.len());
-            format!(
-                "https://www.nosdeputes.fr/depute/photo/{}/60",
-                deputes
-                    .get(index)
-                    .ok_or(anyhow::anyhow!("Can't retrieve deputes at index"))?["depute"]["slug"]
+    if let serde_json::Value::Array(deputes) = &resp["deputes"] {
+        let index = rng.gen_range(0..deputes.len());
+        let slug = deputes
+            .get(index)
+            .ok_or_else(|| anyhow::anyhow!("Can't retrieve deputes at index {index}"))?["depute"]
+            ["slug"]
+            .as_str()
+            .ok_or_else(|| anyhow::anyhow!("No slug for depute index {index}"))?;
+        let url = format!("https://www.nosdeputes.fr/depute/photo/{slug}/250");
+
+        let bytes = reqwest::get(url).await?.bytes().await?;
+
+        msg.channel_id
+            .send_files(
+                &ctx,
+                vec![(bytes.as_ref(), format!("{slug}.png").as_str())],
+                |m| m.content(&msg.author),
             )
-        } else {
-            Err(anyhow::anyhow!("Can't extract deputes from json"))?
-        }
-    });
+            .await?;
+    } else {
+        Err(anyhow::anyhow!("Can't extract deputes from json"))?;
+    }
+    // });
 
     Ok(())
 }
